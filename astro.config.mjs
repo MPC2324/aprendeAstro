504012e1068f643010262902f3697cad674a5b0f
MPC1812 <nose@nose.com>
2024-08-13 14:38:02 +0200

    Configurar base: aprendeAstro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://MPC2324/github.io',
+  base: 'aprendeAstro',
   integrations: [tailwind()]
 });
